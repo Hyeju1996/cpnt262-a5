89299018a4feaf0bf00ef8980f41571ca00248af
helena <helena@helenas-MBP.hitronhub.home>
2023-03-15 16:05:08 -0700

    inzilizing

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let month = prompt("please enter");
+
+const date = ['Mon','Tue', 'Wed', 'Thu',];
+
+const count = date.push('Fri');
+console.log(count);
+console.log(date);
+
+date.push ('Fri','sat', 'sun');
+console.log(date);
+
+for (let i = date; i <= month; i++){
+    if (i === date);{
+
+    }
+console.log(i);
+}
